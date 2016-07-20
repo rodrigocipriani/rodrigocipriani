049e5dca98b7a250f1028061801f1c1f84945a8a
Rodrigo Cipriani <Rodrigo Cipriani>
2016-07-19 22:48:50 -0300

    Estrutura inicial

---
+++ frontend/app/main.ts
@@ -0,0 +1,3 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './portal/app.component';
+bootstrap(AppComponent);
---
+++ frontend/app/portal/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    templateUrl: './app/portal/app.component.html',
+    styleUrls: ['./app/portal/app.component.css']
+})
+export class AppComponent { }
